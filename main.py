bb397b18e59612def0cbf6181593b023dcc55344
Szymon050405 <molenda.szymon05@gmail.com>
2025-06-08 08:38:38 +0200

    feat: Dodanie funkcjonalności przybliżenie do stacji

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import tkintermapview
 from utils import get_fuel_stations_near_city
 
 station_markers = []
+station_data = []  # przechowuje dane stacji z utils
+
 
 def find_stations():
     city = entry_city.get()
@@ -15,6 +17,7 @@ def find_stations():
     for m in station_markers:
         m.delete()
     station_markers.clear()
+    station_data.clear()
 
     stations = get_fuel_stations_near_city(city)
 
@@ -25,11 +28,23 @@ def find_stations():
     for station in stations:
         marker = map_widget.set_marker(station["lat"], station["lon"], text=station["name"])
         station_markers.append(marker)
+        station_data.append(station)  # zapisz stację do listy
         listbox.insert(END, f"{station['name']} – {station['lat']:.5f}, {station['lon']:.5f}")
 
     map_widget.set_position(stations[0]["lat"], stations[0]["lon"])
     map_widget.set_zoom(13)
     label_info.config(text=f"✅ Znaleziono {len(stations)} stacji")
+
+
+def on_listbox_click(event):
+    try:
+        index = listbox.curselection()[0]
+        station = station_data[index]
+        map_widget.set_position(station["lat"], station["lon"])
+        map_widget.set_zoom(16)
+    except IndexError:
+        pass
+
 
 # GUI
 root = Tk()
@@ -52,6 +67,7 @@ label_info.pack(pady=5)
 
 listbox = Listbox(frame_left, width=50)
 listbox.pack(pady=10, fill=BOTH, expand=True)
+listbox.bind("<<ListboxSelect>>", on_listbox_click)
 
 map_widget = tkintermapview.TkinterMapView(frame_right, width=700, height=500)
 map_widget.pack(fill=BOTH, expand=True)
